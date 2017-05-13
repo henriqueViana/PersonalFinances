24161e3975117d51e47844a0b17770308894510f
henriqueviana <henriquehvisa@gmail.com>
2017-05-13 17:00:33 -0300

    Create summary service

--- backend/src/api/payCycle/payCycleService.js
+++ backend/src/api/payCycle/payCycleService.js
@@ -6,8 +6,8 @@ PayCycle.methods(['get', 'post', 'put', 'delete']);
 //Corrige o método put fazendo-o retornar o novo objeto após a alteração e aplica as validaçoes do schema, que por default, funcionanm apenas no método post
 PayCycle.updateOptions({ new: true, runValidators: true });
 
-//Retorna o total de registros no banco
-PayCycle.route('count', (req,res,next) => {
+//Serviço que retorna o total de registros no banco
+PayCycle.route('count', (req, res, next) => {
   PayCycle.count((error, value) => {
     
     if(error) {
@@ -19,5 +19,37 @@ PayCycle.route('count', (req,res,next) => {
   });
 });
 
+//Serviço que retorna o valor total de creditos e de débitos
+PayCycle.route('summary', (req, res, next) => {
+  PayCycle.aggregate(
+    {
+      $project: {
+        credit: { $sum: '$credits.value'}, 
+        debt: { $sum: '$debts.value'}
+      }
+    } , 
+    {
+      $group: {
+        _id: null, 
+        credit: { $sum: '$credit' },
+        debt: { $sum: '$debt' } 
+      }
+    } ,
+    {
+      $project: {
+        _id: 0,
+        credit: 1,
+        debt: 1
+      }
+    } , 
+    (error, result) => {
+      if(error) {
+        res.status(500).json({errors: [error]});
+      } else{
+        res.json(result[0] || {credit: 1, debt: 1});
+      }
+    }
+  );
+});
 
 module.exports = PayCycle;
