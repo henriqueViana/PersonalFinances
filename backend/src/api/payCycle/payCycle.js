91116fe477b57a0dcb7db5ba9638dcdd82a5c4a0
henriqueviana <henriquehvisa@gmail.com>
2017-05-13 15:44:01 -0300

    Tests routes in postman and add service count

--- backend/src/api/payCycle/payCycle.js
+++ backend/src/api/payCycle/payCycle.js
@@ -2,22 +2,22 @@ const restful = require('node-restful');
 const mongoose = restful.mongoose;
 
 const creditSchema = new mongoose.Schema({
-    name: { type: String, required: true } ,
-    value: { type: Number, min: 0, required: true }
+  name: { type: String, required: [true, 'Informe um nome para o crédito'] } ,
+  value: { type: Number, min: 0, required: [true, 'Informe um valor para o crédito'] }
 });
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, required: true } ,
-    value: { type: Number, min: 0, required: true } ,
-    status: { type: String, required: false, uppercase: true , enum: ['PAGO', 'PENDENTE', 'AUSENTE']}
+  name: { type: String, required: [true, 'Informe um nome para o seu débito'] } ,
+  value: { type: Number, min: 0, required: [true, 'Informe um valor para o débito'] } ,
+  status: { type: String, required: false, uppercase: true , enum: ['PAGO', 'PENDENTE', 'AUSENTE']}
 });
 
 const payCycleSchema = new mongoose.Schema({
-    name: { type: String, required: true } ,
-    month: { type: Number, min: 1, max: 12, required: true } ,
-    year: { type: Number, min: 1970, max: 2100, required: true } ,
-    credits: [ creditSchema ] ,
-    debts: [ debtSchema ]
+  name: { type: String, required: [true, 'Informe um nome para o ciclo de pagamento'] } ,
+  month: { type: Number, min: 1, max: 12, required: [true, 'Informe o mês do ciclo de pagamento'] } ,
+  year: { type: Number, min: 1970, max: 2100, required: [true, 'Informe o ano do ciclo de pagameto'] } ,
+  credits: [ creditSchema ] ,
+  debts: [ debtSchema ]
 });
 
 module.exports = restful.model('PayCycle' , payCycleSchema);
--- backend/src/api/payCycle/payCycleService.js
+++ backend/src/api/payCycle/payCycleService.js
@@ -6,4 +6,18 @@ PayCycle.methods(['get', 'post', 'put', 'delete']);
 //Corrige o método put fazendo-o retornar o novo objeto após a alteração e aplica as validaçoes do schema, que por default, funcionanm apenas no método post
 PayCycle.updateOptions({ new: true, runValidators: true });
 
+//Retorna o total de registros no banco
+PayCycle.route('count', (req,res,next) => {
+  PayCycle.count((error, value) => {
+    
+    if(error) {
+      res.status(500).json({errors: [error]});
+    } else {
+      res.json({value});
+    }
+
+  });
+});
+
+
 module.exports = PayCycle;
