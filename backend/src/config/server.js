3711eb77f67e303145cffa12ac52433a5c654c40
henriqueviana <henriquehvisa@gmail.com>
2017-05-13 18:44:46 -0300

    Create and enable cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,9 +2,11 @@ const PORT = 3000;
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const cors = require('./cors');
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
+server.use(cors);
 
 server.listen(PORT, () => console.log(`server is running in port ${PORT}`));
 
