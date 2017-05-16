a502bd13d36820e016c2ccf92f11cfefdc884692
henriqueviana <henriquehvisa@gmail.com>
2017-05-16 14:34:18 -0300

    Finished backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
 const cors = require('./cors');
+const queryParser = require('express-query-int');
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 server.use(cors);
+server.use(queryParser());
 
 server.listen(PORT, () => console.log(`server is running in port ${PORT}`));
 
