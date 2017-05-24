e2a442807b2266ea3a773a7592692d57ba90af0e
henriqueviana <henriquehvisa@gmail.com>
2017-05-24 00:56:13 -0300

    Init frontend and create webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,38 +2,33 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js' ,
+  entry: './src/index.jsx' ,
   output: {
     path: __dirname + '/public' ,
-    filename: 'app.js'
+    filename: './app.js'
   } ,
-
   devServer: {
     port: 8080 ,
     contentBase: './public'
   } ,
-
   resolve: {
-    extnsions: ['','js','jsx'] ,
+    extensions: ['','.js','.jsx'] ,
     alias: {
       modules: __dirname + '/node_modules' ,
       jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js' ,
       bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
     }
   } ,
-
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery' ,
       jQuery: 'jquery' ,
-      'window.jQuery': jquery
+      'window.jQuery': 'jquery'
     }) ,
-
     new ExtractTextPlugin('app.css')
-  ],
-
+  ] ,
   module: {
-    loader: [
+    loaders: [
       {
         test: /.js[x]?$/ ,
         loader: 'babel-loader' ,
@@ -42,17 +37,15 @@ module.exports = {
           presets: ['es2015', 'react'] ,
           plugins: ['transform-object-rest-spread']
         }
-      },
-
+      } ,
       {
         test: /\.css$/ ,
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
       } ,
-
       {
         test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/ ,
         loader: 'file'
       }
-    ]
+    ] 
   }
 }
